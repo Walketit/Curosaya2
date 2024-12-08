2b75c59064bc21279fd09cdc158e813be48598af
DmitryPonomarenko <ponomarenkodmitry05@gmail.com>
2024-12-08 14:54:04 +0700

    Начало проекта

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.cursproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.cursproject to javafx.fxml;
+    exports com.example.cursproject;
+}
